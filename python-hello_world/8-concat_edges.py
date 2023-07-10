e099b7b67f01c6d8bcaca22de48d9b07a43c28a5
kalisa21 <w.kalisa@alustudent.com>
2023-07-10 02:42:04 -0700

    Updated1

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-# 8-uppercase.py
-
-
-def uppercase(str):
-    """Print a string in uppercase."""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("")
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
